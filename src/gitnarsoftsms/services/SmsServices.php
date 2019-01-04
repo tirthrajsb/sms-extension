98202b480891494ee8342d51768ae03bc10a8889
Thirthraj <tirthraj.singh@girnarsoft.com>
2019-01-04 10:33:38 +0530

    SMS extension created

---
+++ src/gitnarsoftsms/services/SmsServices.php
@@ -0,0 +1,20 @@
+<?php
+namespace gitnarsoftsms\services;
+
+/**
+ * SmsServices: Set config for send sms
+ *
+ * @category  PHP
+ * @package   SMS
+ * @author    Tirthraj Singh <tirthraj.singh@girnarsoft.com>
+ * @copyright 2018 GirnarSoft Pvt. Ltd.
+ * @license   2018 GirnarSoft Pvt. Ltd.
+ * @link      http://www.girnarsoft.com
+ */
+class SmsServices implements interfaces\ISmsServices
+{
+    public static function renderForm(): string
+    {
+        return "Render form here";
+    }
+}
---
+++ src/gitnarsoftsms/services/interfaces/ISmsServices.php
@@ -0,0 +1,17 @@
+<?php
+namespace gitnarsoftsms\services\interfaces;
+
+/**
+ * SmsServices: Set config for send sms
+ *
+ * @category  PHP
+ * @package   SMS
+ * @author    Tirthraj Singh <tirthraj.singh@girnarsoft.com>
+ * @copyright 2018 GirnarSoft Pvt. Ltd.
+ * @license   2018 GirnarSoft Pvt. Ltd.
+ * @link      http://www.girnarsoft.com
+ */
+interface ISmsServices
+{
+    public static function renderForm(): string;
+}
